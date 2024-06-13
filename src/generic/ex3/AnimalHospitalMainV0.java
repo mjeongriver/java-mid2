465dcf6c7197684cf128af07a973f99f3e1587f8
mjeongriver <jennygh2@hanmail.net>
2024-06-13 16:55:45 +0900

    타입 매개변수 제한 1 - 시작

---
+++ src/generic/ex3/AnimalHospitalMainV0.java
@@ -0,0 +1,30 @@
+package generic.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV0 {
+
+    public static void main(String[] args) {
+        DogHospital dogHospital = new DogHospital();
+        CatHospital catHospital = new CatHospital();
+
+        Dog dog = new Dog("멍멍이", 20);
+        Cat cat = new Cat("야옹이", 15);
+
+        //개 병원
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        catHospital.set(cat);
+        catHospital.checkup();
+
+        //문제 1: 개 병원에 고양이 전달
+        //dogHospital.set(cat); // 다른 타입 입력: 컴파일 오류
+
+        //문제 2: 개 타입 반환
+        dogHospital.set(dog);
+        Dog biggerDog = dogHospital.bigger(new Dog("뭉뭉이", 10));
+        System.out.println("biggerDog = " + biggerDog);
+    }
+}
