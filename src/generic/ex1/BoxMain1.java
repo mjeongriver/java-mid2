2bb314a41eaa6fc3babcc9e4dd88e7a63e96afc0
mjeongriver <jennygh2@hanmail.net>
2024-06-13 15:48:16 +0900

    다형성을 통한 중복 해결 시도

--- src/generic/ex1/BoxMain1.java
+++ src/generic/ex1/BoxMain1.java
@@ -3,6 +3,8 @@ package generic.ex1;
 public class BoxMain1 {
 
     public static void main(String[] args) {
+
+        //타입 안정성은 높으나 코드 재사용성은 낮음 
         IntegerBox integerBox = new IntegerBox();
         integerBox.set(10); //오토박싱
         Integer integer = integerBox.get();
