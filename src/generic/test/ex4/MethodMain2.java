bfeed68998033d4ec14a69dd2831c98f956a091c
mjeongriver <jennygh2@hanmail.net>
2024-06-18 16:44:26 +0900

    제네릭 메서드 활용

---
+++ src/generic/test/ex4/MethodMain2.java
@@ -0,0 +1,20 @@
+package generic.test.ex4;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class MethodMain2 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("멍멍이", 20);
+        Cat cat = new Cat("야옹이", 15);
+        
+        //매개변수 추론
+        AnimalMethod.checkup(dog);
+        AnimalMethod.checkup(cat);
+
+        Dog targetDog = new Dog("큰 멍멍이", 200);
+        Dog bigger = AnimalMethod.<Dog>bigger(dog, targetDog); //뒤에 cat으로 하면 매개변수 추론이 안됨
+        System.out.println("bigger = " + bigger);
+    }
+}
---
+++ src/generic/test/ex4/MethodMain3.java
@@ -0,0 +1,21 @@
+package generic.test.ex4;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class MethodMain3 {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("멍멍이", 20);
+        Cat cat = new Cat("야옹이", 15);
+
+        ComplexBox<Dog> hospital = new ComplexBox<>();
+        hospital.set(dog);
+
+        //제네릭 메서드가 제네릭 타입보다 우선순위를 가진다.
+        Cat returnCat = hospital.printAndReturn(cat);
+        System.out.println("returnCat = " + returnCat);
+    }
+}
