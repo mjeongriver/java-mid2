3e6e49bc4da9087be1e95a3034b618f0096e50e8
mjeongriver <jennygh2@hanmail.net>
2024-06-20 16:54:30 +0900

    와일드 카드 1

---
+++ src/generic/test/ex5/WildcardEx.java
@@ -0,0 +1,40 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+
+public class WildcardEx {
+
+    static <T> void printGenericV1(Box<T> box) {
+        System.out.println("T = " + box.get());
+    }
+
+    //Box<Dog>, Box<Cat>, Box<Object>
+    //와일드 카드는 만들어진 제네릭 타입을 가져다 쓸 때 사용
+    //와일드 카드 ?는 모든 타입을 받을 수 있다.
+    static void printWildcardV1(Box<?> box) {
+        Object object = box.get(); //비제한 와일드 카드
+        System.out.println("? = " + box.get());
+    }
+
+    static <T extends Animal> void printGenericV2(Box<T> box) {
+        T t = box.get();
+        System.out.println("이름 = " + t.getName());
+    }
+
+    static void printWildcardV2(Box<? extends Animal> box) {
+        Animal animal = box.get();
+        System.out.println("? = " + animal.getName());
+    }
+
+    static <T extends Animal> T printAndReturnGeneric(Box<T> box) {
+        T t = box.get();
+        System.out.println("이름 = " + t.getName());
+        return t;
+    }
+
+    static Animal printAndReturnWildcard(Box<? extends Animal> box) {
+        Animal animal = box.get();
+        System.out.println("이름 = " + animal.getName());
+        return animal;
+    }
+}
