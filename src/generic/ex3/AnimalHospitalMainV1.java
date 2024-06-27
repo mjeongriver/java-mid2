481c083fdf65580271a2cdf3a02e8bbf8ddd0b51
mjeongriver <jennygh2@hanmail.net>
2024-06-27 16:57:50 +0900

    문제와 풀이 2

--- src/generic/ex3/AnimalHospitalMainV1.java
+++ src/generic/ex3/AnimalHospitalMainV1.java
@@ -1,14 +1,13 @@
-package generic.test.ex3;
+package generic.ex3;
 
 import generic.animal.Cat;
 import generic.animal.Dog;
 
-public class AnimalHospitalMainV3 {
+public class AnimalHospitalMainV1 {
 
     public static void main(String[] args) {
-        AnimalHospitalV3<Dog> dogHospital = new AnimalHospitalV3();
-        AnimalHospitalV3<Cat> catHospital = new AnimalHospitalV3();
-      //<Integer> intHospital = new AnimalHospitalV3(); //컴파일 오류
+        AnimalHospitalV1 dogHospital = new AnimalHospitalV1();
+        AnimalHospitalV1 catHospital = new AnimalHospitalV1();
 
         Dog dog = new Dog("멍멍이", 20);
         Cat cat = new Cat("야옹이", 15);
@@ -21,11 +20,11 @@ public class AnimalHospitalMainV3 {
         catHospital.checkup();
 
         //문제 1: 개 병원에 고양이 전달
-        //dogHospital.set(cat); //다른 타입을 입력: 컴파일 오류
+        dogHospital.set(cat); // 매개변수 체크 실패: 컴파일 오류가 발생하지 않음
 
         //문제 2: 개 타입 반환
         dogHospital.set(dog);
-        Dog biggerDog = dogHospital.bigger(new Dog("뭉뭉이", 10));
+        Dog biggerDog = (Dog) dogHospital.bigger(new Dog("뭉뭉이", 10));
         System.out.println("biggerDog = " + biggerDog);
     }
 }
--- src/generic/ex3/CatHospital.java
+++ src/generic/ex3/CatHospital.java
@@ -1,23 +1,22 @@
-package generic.test.ex3;
+package generic.ex3;
 
-import generic.animal.Animal;
+import generic.animal.Cat;
 
-public class AnimalHospitalV3<T extends Animal> {
+public class CatHospital {
 
-    private T animal;
+    private Cat animal;
 
-    public void set(T animal) {
+    public void set(Cat animal) {
         this.animal = animal;
     }
 
     public void checkup() {
-        //컴파일 오류
         System.out.println("동물 이름: " + animal.getName());
         System.out.println("동물 크기: " + animal.getSize());
         animal.sound();
     }
 
-    public T bigger(T target) {
+    public Cat bigger(Cat target) {
         return animal.getSize() > target.getSize() ? animal : target;
     }
 }
