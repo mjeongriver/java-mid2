5c2f76ec8c8aad6ebb54ad8217f3ff835beb434e
mjeongriver <mjeongriver@gmail.com>
2024-07-18 16:52:41 +0900

    문제와 풀이 1

--- src/generic/ex5/EraserBox.java
+++ src/generic/ex5/EraserBox.java
@@ -2,6 +2,7 @@ package generic.ex5;
 
 public class EraserBox<T> {
     public boolean instanceCheck(Object param) {
+        //instanceof는 항상 Object와 비교하게 된다. 이렇게 되면 항상 참이 반환되는 문제가 발생한다.
         //return param instanceof T;
         return false;
     }
