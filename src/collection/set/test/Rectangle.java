8357544ffaf9fbd36a603417ffa8170aeb30360c
mjeongriver <mjeongriver@gmail.com>
2024-09-11 16:04:26 +0900

    문제와 풀이 2

---
+++ src/collection/set/test/Rectangle.java
@@ -0,0 +1,35 @@
+package collection.set.test;
+
+import java.util.Objects;
+
+public class Rectangle {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) { //hashcode랑 같이 추가 했을 때 중복 안나옴
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return width == rectangle.width && height == rectangle.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height);
+    }
+}
